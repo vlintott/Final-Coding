458a56c5ab16e9fce94ee70dc9cfac0bd81c6875
vlintott <clv@udel.edu>
2018-12-20 00:00:06 -0500

    Update Retirement.java

--- FinalExamProject/src/main/java/pkgCore/Retirement.java
+++ FinalExamProject/src/main/java/pkgCore/Retirement.java
@@ -10,6 +10,60 @@ public class Retirement {
 	private double dMonthlySSI;
 	
 	//TODO: Build the contructor, getters and setters for the attributes above.
+	Retirement(int iYTW, double aRW,int iYR, double aRR, double dRI,double dMSSI) {
+	
+		iYearsToWork = iYTW;
+		dAnnualReturnWorking = aRW;
+		iYearsRetired= iYR;
+		dAnnualReturnRetired = aRR;
+		dRequiredIncome = dRI;
+		dMonthlySSI = dMSSI;
+	}	
+	public int getiYearsToWork() {
+		return iYearsToWork;
+	}
+	
+	public void setiYearsToWork(int iYTW) {
+		this.iYearsToWork = iYTW;
+	}
+		
+	public double getdAnnualReturnWorking() {
+		return dAnnualReturnWorking;
+	}
+	
+	public void setdAnnualReturnWorking(double aRW) {
+		this.dAnnualReturnWorking = aRW;
+	}
+	
+	public int getiYearsRetired() {
+		return iYearsRetired;
+	}
+	
+	public void setiYearsRetired(int iYR) {
+		this.iYearsRetired = iYR;
+	}
+	
+	public double getdAnnualReturnRetired () {
+		return dAnnualReturnRetired;
+	}
+	public void setdAnnualReturnRetired(double aRR) {
+		this.dAnnualReturnRetired= aRR;
+	}
+	
+	public double getdRequiredIncome() {
+		return dRequiredIncome;
+	}
+	public void setdRequiredIncome(double dRI) {
+		this.dRequiredIncome= dRI;
+	}
+	
+	public double getdMonthlySSI() {
+		return dMonthlySSI;
+	}
+	
+	public void setdMothlySSI(double dMSSI) {
+		this.dMonthlySSI= dMSSI;
+	}
 	
 	public double AmountToSave()
 	{
@@ -17,7 +71,7 @@ public class Retirement {
 		//		and Annual return while working
 		
 		
-		return 0;
+		return (1454485.55*(1/((1+dAnnualReturnWorking)Math.pow(iYearsToWork))))/(12*iYearsToWork);
 	}
 	
 	public double TotalAmountSaved()
@@ -25,6 +79,6 @@ public class Retirement {
 		//	TODO: Determine amount to be saved based on Monthly SSI, Required Income, Annual return during retirement
 		//		and number of years retired.
 		//
-		return 0;
+		return (iYearsRetired*dRequiredIncome)-(iYearsRetired*dMonthlySSI);
 	}
 }
