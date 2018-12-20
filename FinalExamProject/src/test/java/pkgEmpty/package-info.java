64f2fbfff7a715808b140af1600c4c89b449c9f1
vlintott <clv@udel.edu>
2018-12-20 00:21:34 -0500

    Update package-info.java

--- FinalExamProject/src/test/java/pkgEmpty/package-info.java
+++ FinalExamProject/src/test/java/pkgEmpty/package-info.java
@@ -85,15 +85,75 @@ public class testRetirement{
 				  instance.getiYearsRetired(20);
 				  int years = instance.getiYearsRetired();
 				  assertEquals(years, iYR);
+				
+				@Test
+				public void testSetdAnnualReturnWorking() {
+				  System.out.println("dAnnualReturnWorking");
+				  int dARW= 0.02;
+				  dAnnualReturnWorking instance = new dAnnualReturnWorking();
+				  assertEquals(instance.getdAnnualReturnWorking(), dARW));
+				}
+
+				@Test
+				public void testGetdAnnualReturnWorking() {
+				  System.out.println("getdAnnualReturnWorking");
+				  dAnnualReturnWorking instance = new dAnnualReturnWorking();
+				  int dARW=0.02;
+				  instance.getdAnnualReturnWorking(0.02);
+				  int return = instance.getdAnnualReturnWorking();
+				  assertEquals(return, dARW);
+					
+				@Test
+				public void testSetdRequiredIncome() {
+				  System.out.println("dRequiredIncome");
+				  int dRI= 10000;
+				  dRequiredIncome instance = new dRequiredIncome();
+				  assertEquals(instance.getdRequiredIncome(), dRI));
+				}
+
+				@Test
+				public void testGetdRequiredIncome() {
+				  System.out.println("getdRequiredIncome");
+				  dRequiredIncome instance = new dRequiredIncome();
+				  int dRI=10000;
+				  instance.getdRequiredIncome(10000);
+				  int income = instance.getdRequiredIncome();
+				  assertEquals(income, dRI);
+					
+				@Test
+				public void testSetdMonthlySSI() {
+				  System.out.println("dMonthlySSI");
+				  int dMSSI= 2642;
+				  dMonthlySSI instance = new dMonthlySSI();
+				  assertEquals(instance.getdMonthlySSI(), dMSSI));
+				}
+
+				@Test
+				public void testGetdMonthlySSIe() {
+				  System.out.println("dMonthlySSI");
+				  dMonthlySSI instance = new dMonthlySSI();
+				  int dMSSI=2642;
+				  instance.getdMonthlySSI(2642);
+				  int month = instance.getdMonthlySSI();
+				  assertEquals(month, dMSSI);
 
 				@Test
 				public void testAmountToSave() {
 					System.out.println("AmountToSave");
 					AmountToSave instance = new AmountToSave();
-					int ATS=1454485.55;
-					instance.AmountToSave(1454485.55);
+					int ATS=554.13;
+					instance.AmountToSave(554.13);
 					int amount = instance.AmountToSave();
-					assertEquals(years, ATS);
+					assertEquals(amount, ATS);
 				}
+				
+				@Test
+				public void testTotalAmountSaved() {
+					System.out.println("TotalAmountSaved");
+					TotalAmountSaved instance = new TotalAmountSaved();
+					int TAS=1454485.55;
+					instance.TotalAmountSaved(1454485.55);
+					int amount = instance.TotalAmountSaved();
+					assertEquals(amount, TAS);
 	}
 }
